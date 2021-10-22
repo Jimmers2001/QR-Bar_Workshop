35324138abc0391ba3f818568589ec9c884d72fd
Jimmers2001 <james.d.batista@gmail.com>
2021-10-22 15:41:43 -0400

    full QRCode creation

--- QR_creation.py
+++ QR_creation.py
@@ -1,29 +1,30 @@
 import qrcode
 
 #function that takes data, encodes it into the QR code, and returns the png file of the QR Code
-def create_QR(input):
+def create_QR(text, file_name, qr_version):
     #Create the QR Code object
     qr = qrcode.QRCode(
-        version = 1,
+        version = qr_version,
         error_correction = qrcode.constants.ERROR_CORRECT_L, #L, M, Q, H
         box_size = 10,
         border=5
     )
 
-    qr.add_data(input)
+    qr.add_data(text) #encode the inputted text into the qrcode
     qr.make(fit=True)
 
     img = qr.make_image(fill='black', back_color='white')
-    img.save('my_qrcode.png') #name of qrcode file to save it to
+    img.save(file_name) #name of qrcode file to save it to
 
     return img
 
 if __name__ == "__main__":
-    text1 = "crab"
-    text2 = "schnozz"
-    text3 = "meg"
+    qr1_text = "Testing text 1"
+    qr1_file = "my_qr1.png"
 
-    qr1 = create_QR(text1)
-    qr2 = create_QR(text2)
-    qr3 = create_QR(text3)
+    qr2_text = "Testing text 2"
+    qr2_file = "my_qr2.png"
+
+    qr1 = create_QR(qr1_text, qr1_file, 1)
+    qr2 = create_QR(qr2_text, qr2_file, 40)
     
